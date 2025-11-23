0408d0d2e1e450d70f3aab69c421b84da0be469b
jagritsen93-cmd <jagritsen93@gmail.com>
2025-11-23 06:53:29 +0545

    first commit

---
+++ og.R
@@ -0,0 +1,126 @@
+for (x in 1:20){
+  print(x)
+}
+
+#this is a comment pls
+
+name <-"JACK"
+age <-69
+
+name
+age
+
+
+
+t<-"OG"
+paste("I AM",t)
+
+paste (t,name)
+
+
+num1<-99
+num2<-10
+
+num1+num2
+
+var1<-9
+var2<-11
+var3<-64
+
+
+var1
+var2
+var3
+
+
+my_var <-60
+my_var <-"NICE BRO"
+
+
+
+x<-11.99
+class(x)
+
+
+x<-1000L
+class(x)
+
+
+
+y<- 9i+3
+class(y)
+
+y<-TRUE
+class(y)
+
+
+class(t)
+
+
+
+
+x<-11
+x
+
+x<-1L
+y<-2
+a<-as.numeric(x)
+x
+
+
+
+min(11,232,342234)
+
+
+sqrt(169)
+
+
+abs(-88.5)
+
+
+ceiling(1.4)
+floor(9.9)
+
+
+plot (1:10)
+
+str<- 'nice'
+str
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+cat(str)
+
+
+
+str<-'helloworld'
+str1<-"ILLAYA THALAPTHY"
+paste(str,str1)
+
+
+str<- "we are \"vikings\",from the south"
+str
+cat(str)
+
+
+a<-200
+b<-201
+if (b>a){
+  print("b is greater than a")
+} else 
+  print("b is not greater than a")
+  
+  
+  
+
+a<-1001
+b<-2001
+if (a<b){
+  print("a is less than b")
+} else {
+  print("b is less than a")
+  
+}
